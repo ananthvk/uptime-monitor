43040e612bab7d084b27917f8e27f84c67695061
ananthvk <mailstoanantha@gmail.com>
2025-01-17 23:42:59 +0530

    Add logger to nestjs application

---
+++ backend/src/logger.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+
+import { Request, Response, NextFunction } from 'express';
+
+// https://github.com/julien-sarazin/nest-playground/issues/1#issuecomment-682588094
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('Content-Length');
+
+      this.logger.log(`[${ip}] ${method} ${originalUrl} ${statusCode} - ${contentLength}`);
+    });
+
+    next();
+  }
+}
