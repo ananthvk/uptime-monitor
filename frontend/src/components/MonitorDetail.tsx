9484001eac8426495379c7bb63262ed6a5781ded
ananthvk <mailstoanantha@gmail.com>
2025-01-19 03:22:15 +0530

    Finished create form

--- frontend/src/components/MonitorDetail.tsx
+++ frontend/src/components/MonitorDetail.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router"
 import axiosClient from "../axios-client";
 import { useQuery } from "react-query";
+import { Box, Button } from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
 
 type Monitor = { id: string, name: string, url: string, port: string, type: string }
 
@@ -28,17 +30,37 @@ function MonitorDetail() {
         return <div>Error occured while fetching data from server {(error as any).message} </div>
     }
 
-    return <div>
-        <h1>
-            {monitor?.name}
-        </h1>
+    return <Box
+        sx={{
+            width: {
+                xs: '90%',
+                sm: '80%',
+                md: '50%',
+                lg: '40%',
+                xl: '30%'
+            }
+        }}
+        marginLeft="1em">
+        <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
+            <h1>
+                {monitor?.name}
+            </h1>
+            <Box>
+                <Button>
+                    <Edit />
+                </Button>
+                <Button>
+                    <Delete />
+                </Button>
+            </Box>
+        </Box>
         <p>
             {monitor?.type}
         </p>
         <p>
             {monitor?.url}:{monitor?.port}
         </p>
-    </div>
+    </Box>
 }
 
 
